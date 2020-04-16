46fcbff7b5672c51ede09e58302babd0b50eb8e9
Vitaly Tokarev <tokarev.exceedteam@gmail.com>
2020-04-16 23:37:49 +0300

    commit 16/04/2020

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-let port = process.argv[2] || process.env.port;
+let port = process.argv[2] || process.env.PORT;
 
 app.use(bodyParser.json());
 
@@ -16,28 +16,32 @@ app.get("/Object",  (request, response) => {
 
 app.post("/Object",  (request, response) => {
     if(!request.body) return response.sendStatus(400);
+
     users.push(request.body);
+    response.sendStatus(200);
 });
 
 app.put("/Object",  (request, response) => {
     if(!request.body) return response.sendStatus(400);
 
-    let editIndex = users.findIndex(m => m.id === request.body.id)
+    const editIndex = users.findIndex(m => m.id === request.body.id)
 
     if(editIndex!==-1) {
         users[editIndex].value = request.body.value;
         users[editIndex].type = request.body.type;
         users[editIndex].fruit = request.body.fruit;
+        response.sendStatus(200);
     }
 });
 
 app.delete("/Object",  (request, response) => {
     if(!request.body) return response.sendStatus(400);
 
-    let editIndex = users.findIndex(m => m.id === request.body.id)
+    const editIndex = users.findIndex(m => m.id === request.body.id)
 
     if(editIndex !==-1) {
         users.splice( editIndex, 1);
+        response.sendStatus(200);
     }
 });
 
