51839880d33ee3bf3c9be2eb46007fbc22e87fe0
unknown <tokarev.exceedteam@gmail.com>
2020-05-06 19:53:50 +0300

    commit 06.05.2020

--- Models/User.js
+++ Models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose'),
   Schema = mongoose.Schema,
-  Object = require('./Object');
+  Object = require('./Object'),
+  asyncHandler = require('express-async-handler');
 
 const UserModelSchema = new Schema({
   name: {
@@ -30,13 +31,16 @@ const UserModelSchema = new Schema({
 });
 
 UserModelSchema.pre('findOneAndDelete', async function(next) {
+  const user = await this.model.findOne({_id: this.getQuery()._id});
+  if ( !user ) { return next('User is not found!'); }
+
   const objects = await Object.find({user: this.getQuery()._id});
+
   if ( objects.length === 0 ) { return next(); }
 
-  for(object of objects) {
+  for (object of objects) {
     object.remove();
   }
-  
   return next();
 });
 
